6bfb848c749dd4738212f105a0f9d975e676b373
Diablohu <diablohudream@gmail.com>
2015-10-22 12:35:30 +0700

    pan gesture

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,96 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var babel = require("gulp-babel");
 
+gulp.task('default',[
+	'job-2015-09-muji_v2'
+])
+
+
+
+
+
+
+
+
+// v2
+
+gulp.task('job-2015-09-muji_v2-js', function(){
+	return gulp.src([
+			'./source/js/!.js',
+
+			'./node_modules/q/q.js',
+			'./source/js/library/jquery-2.1.4.js',
+			'./node_modules/hammerjs/hammer.js',
+			'./node_modules/jquery-hammerjs/jquery.hammer.js',
+			
+			'./source/js/base.js',
+			'./source/js/main.v2.js',
+
+			'./source/js/scenes/photos.js',
+			'./source/js/scenes/timeline.js',
+			'./source/js/scenes/ad.js',
+			'./source/js/scenes/summary.js'
+		])
+		.pipe(concat('js.v2.js'))
+		.pipe(babel({
+			'highlightCode':	false,
+			'comments':			false,
+			'compact':			true,
+			'ast':				false
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./assets'))
+})
+
+gulp.task('job-2015-09-muji_v2-css', function(){
+	return gulp.src([
+			'./source/css.v2.less'
+		])
+		.pipe(less())
+		.pipe(minifyCSS())
+		.pipe(postcss([
+			autoprefixer({
+				'browsers': [
+					'Android >= 2',
+					'Chrome >= 25',
+					'Firefox >= 20',
+					'ie >= 11',
+					'Edge >= 12',
+					'iOS >= 5',
+					'ChromeAndroid >= 25',
+					'ExplorerMobile >= 11'
+				]
+			})
+		]))
+		.pipe(gulp.dest('./assets'))
+})
+
+gulp.task('job-2015-09-muji_v2-watch', function(){
+	gulp.watch(
+			'./source/**/*.js',
+			['job-2015-09-muji_v2-js']
+		)
+	gulp.watch(
+			'./source/**/*.less',
+			['job-2015-09-muji_v2-css']
+		)
+})
+
+gulp.task('job-2015-09-muji_v2',[
+	'job-2015-09-muji_v2-js',
+	'job-2015-09-muji_v2-css',
+	'job-2015-09-muji_v2-watch'
+])
+
+
+
+
+
+
+
+
+// v1
+
 gulp.task('job-2015-09-muji-js', function(){
 	return gulp.src([
 			'./source/js/!.js',
@@ -70,7 +160,7 @@ gulp.task('job-2015-09-muji-watch', function(){
 		)
 })
 
-gulp.task('default',[
+gulp.task('job-2015-09-muji_v1',[
 	'job-2015-09-muji-js',
 	'job-2015-09-muji-css',
 	'job-2015-09-muji-watch'
---
+++ source/js/main.v2.js
@@ -0,0 +1,120 @@
+/* global $ */
+
+var $html, $body, $main
+	,scene_name = []
+	,scene_els = []
+	,scenes = {}
+	,scene_el_cur
+
+
+
+
+
+
+
+window.onload = (function(){
+	$html = $('html')
+	$body = $('body')
+	$main = $body.find('main')
+				.on('touchstart', function(){
+					$main.removeClass('is-scrollhint')
+				})
+
+	// make indicator input element for each scene
+	// cache scene name
+	// empty scene element when changed to next scene
+		var $indicators = $('<div class="indicators"/>').appendTo($main)
+		scene_els = $main.find('.scene')
+		scene_els.each(function(i, scene){
+			scene = $(scene)
+			$('<span/>',{
+				'data-scene':	i+1
+			}).appendTo($indicators)
+			scene_name[i] = scene.attr('data-scene-name')
+			scene.on('transitionend webkitTransitionEnd', function(e){
+				if( e.currentTarget == e.target
+					&& e.originalEvent.propertyName == 'opacity'
+					&& parseFloat(scene.css('opacity')) === 0
+				){
+					scene.empty()
+				}
+			})
+		})
+	
+	// scroll hint
+		$('<div class="scrollhint animated flash"/>').appendTo($main)
+	
+	// remove loader when loaded (opacity transition to 0)
+		var $loader = $main.find('.loader')
+		$loader.on('transitionend webkitTransitionEnd', function(e){
+			if( e.currentTarget == e.target
+				&& e.originalEvent.propertyName == 'opacity'
+				&& parseFloat($loader.css('opacity')) === 0
+			){
+				$loader.remove()
+			}
+		})
+	
+	// next scene button
+		var $nextscene = $main.find('button.nextscene')
+		$nextscene.on('click', function(){
+			$body.trigger('scenenext')
+		})
+
+	// bind custom events
+		$body.on({
+			'scenechange': function(e, scenenumber){
+				if( !scenenumber )
+					return
+				$main.attr('data-scene', scenenumber)
+					.attr('data-scene-name', scene_name[scenenumber-1])
+				$body.removeClass('is-ready-nextscene')
+				$main.removeClass('is-scrollhint')
+				
+				scene_el_cur = scene_els.eq(scenenumber-1)
+				scene_el_cur.css('transform', '')
+				
+				// run specific scene function
+					if( scenes[ scene_name[scenenumber-1] ] )
+						scenes[ scene_name[scenenumber-1] ]( scene_el_cur )
+			},
+			'sceneprev': function(){
+				var cur = parseInt($main.attr('data-scene') || 0)
+				if( cur > 0 )
+					$body.trigger('scenechange', [cur - 1])
+			},
+			'scenenext': function(){
+				var cur = parseInt($main.attr('data-scene') || 0)
+				if( cur < scene_name.length - 1 )
+					$body.trigger('scenechange', [cur + 1])
+			},
+			'scenefinish': function(){
+				$nextscene.trigger('blur')
+				if( $main.attr('data-scene') < scene_name.length )
+					$body.addClass('is-ready-nextscene')
+			}
+		})
+
+	setTimeout(function(){
+		$body.addClass('is-loaded')
+		$body.trigger('scenenext')
+	
+		// event listeners (hammer.js)
+			$body.hammer().bind('panmove', function(e){
+				/*$main.css('left', 'calc(50% '
+						+ (e.gesture.deltaX >=0 ? '+' : '-')
+						+ ' '
+						+ Math.abs(e.gesture.deltaX) + 'px)')*/
+				scene_el_cur.css('transform', 'translateX('+e.gesture.deltaX+'px)')
+			}).bind('panend pancancel', function(e){
+				console.log('panend ' + e.gesture.deltaX)
+				//$main.css('left', '50%')
+				if( e.gesture.deltaX > 50)
+					$body.trigger('sceneprev')
+				else if( e.gesture.deltaX < -50)
+					$body.trigger('scenenext')
+				else
+					scene_el_cur.css('transform', '')
+			})
+	}, 1000)
+})
