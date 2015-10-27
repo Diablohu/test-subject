f7e8172cc3fe5fd7050809b0d10d9e89f68bb00a
Diablohu <diablohudream@gmail.com>
2015-10-27 23:06:54 +0800

    more polish

--- source/js/scenes/entry.js
+++ source/js/scenes/entry.js
@@ -6,7 +6,7 @@ scenes.entry = function($scene){
 		,container = $('<div/>').prependTo($scene)
 		
 	$('<p class="center animated fadeIn"/>')
-		.html('无印十年光阴，与您相伴同行。<br />良人正在招募，期待您的加入。')
+		.html('　无印十年光阴，与您相伴同行。<br />　良人正在招募，期待您的加入。')
 		.appendTo(container)
 
 	// 开始异步函数链
--- source/js/scenes/summary.js
+++ source/js/scenes/summary.js
@@ -3,103 +3,40 @@
 
 scenes.summary = function($scene){
 	var promise_chain 	= Q.fcall(function(){})
-		,$title = $('<h2><span class="animated fadeIn">梦，</span></h2>').appendTo($scene)
-		,$title2
+		,container = $('<div/>')
+						.html(`
+						<h2>
+							<span class="animated fadeInUp">　梦，</span>
+							<span class="animated fadeInUp">　成长，</span>
+							<span class="animated fadeInUp">　在路上。</span>
+						</h2>
+						<h2>
+							<span class="animated fadeInUp">　十年无印，</span>
+							<span class="animated fadeInUp">　良人招募。</span>
+						</h2>
+						`)
+						.appendTo($scene)
+	
+	$('<p class="final animated fadeIn">储备干部 全国招聘</p>')
+		.on('animationend webkitAnimationEnd', function(e){
+			if( e.currentTarget == e.target )
+				setTimeout(function(){
+					$body.trigger('scenefinish')
+				}, 500)
+		})
+		.appendTo(container)
+	
+	/*
 
 	// 开始异步函数链
 		promise_chain
 
 		.then(function(){
-			var deferred = Q.defer()
-			setTimeout(function(){
-				deferred.resolve()
-			}, 1000)
-			return deferred.promise
-		})
-		
-		.then(function(){
-			return $('<span class="animated fadeIn">成长，</span>').appendTo($title)
-		})
-
-		.then(function(){
-			var deferred = Q.defer()
-			setTimeout(function(){
-				deferred.resolve()
-			}, 750)
-			return deferred.promise
-		})
-		
-		.then(function(){
-			return $('<span class="animated fadeIn">在路上。</span>').appendTo($title)
-		})
-
-		.then(function(){
-			var deferred = Q.defer()
-			setTimeout(function(){
-				deferred.resolve()
-			}, 1250)
-			return deferred.promise
-		})
-		
-		.then(function(){
-			$title2 = $('<h2><span class="animated fadeIn">十年无印，</span></h2>').appendTo($scene)
-			return $title2
-		})
-
-		.then(function(){
-			var deferred = Q.defer()
-			setTimeout(function(){
-				deferred.resolve()
-			}, 750)
-			return deferred.promise
-		})
-		
-		.then(function(){
-			return $('<span class="animated fadeIn">良人招募。</span>').appendTo($title2)
-		})
-
-		.then(function(){
-			var deferred = Q.defer()
-			setTimeout(function(){
-				deferred.resolve()
-			}, 1000)
-			return deferred.promise
-		})
-		
-		.then(function(){
-			return $('<p class="final animated fadeIn">储备干部 全国招聘</p>').appendTo($scene)
-		})
-		
-		/*
-		.then(function(){
-			var deferred = Q.defer()
-			setTimeout(function(){
-				deferred.resolve()
-			}, 1000)
-			return deferred.promise
-		})
-		.then(function(){
-			return $('<p class="center animated fadeIn"/>')
-				.append(
-					$('<a/>',{
-						'class':	'nextscene',
-						'href':		'http://zp.muji.com.cn',
-						'target':	'_blank',
-						'html':		'点击了解更多'
-					})
-				).appendTo($scene)
-		})
-		*/
-
-		.then(function(){
-			var deferred = Q.defer()
-			setTimeout(function(){
-				deferred.resolve()
-			}, 1000)
-			return deferred.promise
+			return Q.sleep(1000 + 750 + 750 + 1250 + 750 + 1000 + 1000)
 		})
 		
 		.done(function(){
 			$body.trigger('scenefinish')
 		})
+		*/
 }
